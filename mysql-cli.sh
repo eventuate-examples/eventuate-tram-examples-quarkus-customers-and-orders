0f5aa9504d37b2f433b898124cce315f31e7bc98
Chris Richardson <chris@chrisrichardson.net>
2022-06-07 16:14:41 +0900

    Specified container timeout, upgraded mysql-cli

--- mysql-cli.sh
+++ mysql-cli.sh
@@ -1,6 +1,7 @@
 #! /bin/bash -e
 
-docker run $* \
+docker run ${1:--it} \
    --name mysqlterm --network=${PWD##*/}_default --rm \
-   mysql:5.7.13 \
-   sh -c 'exec mysql -h mysql  -uroot -prootpassword -o eventuate'
+   -e MYSQL_HOST=mysql \
+   mysql/mysql-server:8.0.27-1.2.6-server \
+   sh -c 'exec mysql -h"$MYSQL_HOST"  -uroot -prootpassword -o eventuate'
